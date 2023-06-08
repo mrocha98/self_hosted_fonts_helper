d10350f03b6ecb3a735aebd7abcfda6864bf03cf
Matheus Rocha <matheus_rocha98@outlook.com>
2023-06-07 22:35:55 -0300

    feat: optional expiration in KeyValueStorage

--- lib/src/core/database/key_value_storage/key_value_storage.dart
+++ lib/src/core/database/key_value_storage/key_value_storage.dart
@@ -3,8 +3,12 @@ abstract interface class KeyValueStorage {
   /// else return null
   Future<V?> get<V>(String key);
 
+  /// search [key] in storage and return the associated list if exists
+  /// else return null
+  Future<List<V>?> getList<V>(String key);
+
   /// store the [value] in the [key] index
-  Future<void> set<V>(String key, V? value);
+  Future<void> set<V>(String key, V? value, {DateTime? expiresOn});
 
   /// checks if the [key] contains a non-nullable value
   Future<bool> hasData(String key);
--- lib/src/core/database/key_value_storage/key_value_storage_get_storage_impl.dart
+++ lib/src/core/database/key_value_storage/key_value_storage_get_storage_impl.dart
@@ -1,16 +1,57 @@
+import 'package:clock/clock.dart';
 import 'package:get_storage/get_storage.dart';
 import 'key_value_storage.dart';
 
 class KeyValueStorageGetStorageImpl implements KeyValueStorage {
   @override
-  Future<V?> get<V>(String key) async => GetStorage().read<V?>(key);
+  Future<V?> get<V>(String key) async {
+    final expirationDate = _getExpirationDate(key);
+    if (expirationDate != null && _expired(expirationDate)) {
+      await remove(key);
+      return null;
+    }
+    return GetStorage().read<V>(key);
+  }
 
   @override
-  Future<bool> hasData(String key) async => GetStorage().hasData(key);
+  Future<List<V>?> getList<V>(String key) async {
+    final content = await get<Iterable<dynamic>>(key);
+    if (content == null) return null;
+    return List<V>.from(content);
+  }
 
   @override
-  Future<void> remove(String key) async => GetStorage().remove(key);
+  Future<bool> hasData(String key) =>
+      get<dynamic>(key).then((value) => value != null);
 
   @override
-  Future<void> set<V>(String key, V? value) => GetStorage().write(key, value);
+  Future<void> remove(String key) => Future.wait([
+        GetStorage().remove(key),
+        GetStorage().remove(_getExpirationKey(key)),
+      ]);
+
+  @override
+  Future<void> set<V>(String key, V? value, {DateTime? expiresOn}) async {
+    await GetStorage().write(key, value);
+    if (expiresOn != null) {
+      await GetStorage().write(
+        _getExpirationKey(key),
+        expiresOn.toIso8601String(),
+      );
+    }
+  }
+
+  String _getExpirationKey(String key) => '$key-expiration';
+
+  DateTime? _getExpirationDate(String key) {
+    final expirationKey = _getExpirationKey(key);
+    final expirationDate = GetStorage().read<String>(expirationKey);
+    if (expirationDate != null) {
+      return DateTime.tryParse(expirationDate);
+    }
+    return null;
+  }
+
+  bool _expired(DateTime expirationDate) =>
+      expirationDate.isBefore(clock.now());
 }
